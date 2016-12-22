9d5cd6c275a0be5ebea0400e51a4c28589913eca
Yannick Van Hecke <yannick.vanhecke@telenet.be>
2016-12-22 10:34:18 +0100

    Initial commit: creating first en second activity for the activity counter app.

---
+++ Activities/app/src/main/java/be/yannickvanhecke/activities/activity_activity1.java
@@ -0,0 +1,15 @@
+package be.yannickvanhecke.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_activity1 extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity1);
+    }
+}
